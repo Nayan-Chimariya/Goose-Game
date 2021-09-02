668dc09aaee1336bf881068901ef758d5192e0ac
Nayan <cnayan789@gmail.com>
2021-09-02 20:09:10 +0545

    movement msg added

--- gooseGame.py
+++ gooseGame.py
@@ -1,5 +1,7 @@
 from math import fabs
 from random import randint
+import time
+
 first_to_go = ""
 cycle = 0
 
@@ -26,22 +28,28 @@ def who_goes_first():
     computer_roll = computer_dice_one + computer_dice_two
     print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
 
+  time.sleep(0.5)
+
   if user_roll > computer_roll:
     first_to_go = "Human"
     second_to_go = "Computer"
     print("🏁 Human goes first\n")
+    time.sleep(0.5)
     
   elif computer_roll > user_roll:
     first_to_go = "Computer"
     second_to_go = "Human"
     print("🏁 Computer goes first\n")
+    time.sleep(0.5)
 
   else:
     print("🎌 Tied...\nRe-running the rolls...")
+    time.sleep(0.5)
     who_goes_first()
 
 
 def game_start():
+
   print(f"Its {first_to_go}'s turn\n")
   game_start = input("Press Enter: ")
   if game_start == "":
@@ -50,12 +58,22 @@ def game_start():
       user_dice_two = randint(1,6)
       user_roll = user_dice_one + user_dice_two
       print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+      time.sleep(1.5)
+
+      print(f"\nMoving you to position {user_roll} ...")
+      time.sleep(1)
+      print(f"Human is now in position {user_roll}\n")
 
     else:
       computer_dice_one = randint(1,6)
       computer_dice_two = randint(1,6)
       computer_roll = computer_dice_one + computer_dice_two
       print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+      time.sleep(1.5)
+
+      print(f"\nMoving you to position {computer_roll} ...")
+      time.sleep(1)
+      print(f"Computer is now in position {computer_roll}\n")
       
 
   print(f"Its {second_to_go}'s turn\n")
@@ -66,24 +84,38 @@ def game_start():
       user_dice_two = randint(1,6)
       user_roll = user_dice_one + user_dice_two
       print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+      time.sleep(1.5)
+
+      print(f"\nMoving you to position {user_roll} ...")
+      time.sleep(1)
+      print(f"Human is now in position {user_roll}\n")
       
     else:
       computer_dice_one = randint(1,6)
       computer_dice_two = randint(1,6)
       computer_roll = computer_dice_one + computer_dice_two
       print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+      time.sleep(1.5)
+
+      print(f"\nMoving you to position {computer_roll} ...")
+      time.sleep(1)
+      print(f"Computer is now in position {computer_roll}\n")
       
 
 if start == "p":
   who_goes_first()
+  time.sleep(0.5)
   print("-------------------")
   print("🎮 Game begins 🎮")
   print("-------------------\n")
+  time.sleep(1)
+
 
   game_start_state = True
 
   while game_start_state != False:
     game_start()
+    time.sleep(1.5)
     
 else:
   print("\nGood Game\n")
