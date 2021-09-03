4b35687a6b5cc7a838f73335225ee6eb8eed273e
Nayan <cnayan789@gmail.com>
2021-09-03 13:55:51 +0545

    added game stop conditions and minor bug fixes

--- gooseGame.py
+++ gooseGame.py
@@ -16,14 +16,15 @@ start = input("Press 'p' to play\nPress 'q' to quit: ").lower()
 def who_goes_first():
   global first_to_go
   global second_to_go
-  doesUser_enter = input("\nHuman player press Enter: ")
+
+  doesUser_enter = input("\n👨 Human player press Enter: ")
   if doesUser_enter == "":
     user_dice_one = randint(1,6)
     user_dice_two = randint(1,6)
     user_roll = user_dice_one + user_dice_two
     print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
 
-  doesComputer_enter = input("Computer press Enter: ")
+  doesComputer_enter = input("🤖 Computer press Enter: ")
   if doesComputer_enter == "":
     computer_dice_one = randint(1,6)
     computer_dice_two = randint(1,6)
@@ -53,7 +54,8 @@ def who_goes_first():
 def game_start():
   global user_position 
   global computer_position 
-  print(f"Its {first_to_go}'s turn\n")
+
+  print(f"🟢 Its {first_to_go}'s turn\n")
   game_start = input("Press Enter: ")
   if game_start == "":
     if first_to_go == "Human":
@@ -61,27 +63,68 @@ def game_start():
       user_dice_two = randint(1,6)
       user_roll = user_dice_one + user_dice_two
       user_position += user_roll
-      print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
-      time.sleep(1.5)
 
-      print(f"\nMoving you to position {user_position} ...")
-      time.sleep(1)
-      print(f"Human is now in position {user_position}\n")
+      if user_position > 24:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(0.5)
+        print("🛑 Exceeded board limit... you did not move")
+        user_position -= user_roll
+        time.sleep(0.5)
+        print(f"Your current position is {user_position}\n")
+
+      elif user_position == 24:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(1.5)
+        print(f"\nMoving you to position {user_position} ...")
+        time.sleep(1)
+        print(f"Human is now in position {user_position}\n")
+
+        print("🏆 Congrats human you have won")
+        print("I guess there is hope for humatiy after all\n")
+        exit(0)
+      
+      else:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(1.5)
+
+        print(f"\nMoving you to position {user_position} ...")
+        time.sleep(1)
+        print(f"Human is now in position {user_position}\n")
 
     else:
       computer_dice_one = randint(1,6)
       computer_dice_two = randint(1,6)
       computer_roll = computer_dice_one + computer_dice_two
       computer_position += computer_roll
-      print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
-      time.sleep(1.5)
+      if computer_position > 24:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(0.5)
+        print("🛑 Exceeded board limit... you did not move")
+        computer_position -= computer_roll
+        time.sleep(0.5)
+        print(f"Your current position is {computer_position}\n")
 
-      print(f"\nMoving you to position {computer_position} ...")
-      time.sleep(1)
-      print(f"Computer is now in position {computer_position}\n")
+      elif computer_position == 24:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(1.5)
+        print(f"\nMoving you to position {computer_position} ...")
+        time.sleep(1)
+        print(f"Computer is now in position {computer_position}\n")
+
+        print("🏆 Congrats computer you have won")
+        print("Now human termination is certain\n")
+        exit(0)
+        
+      else:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(1.5)
+
+        print(f"\nMoving you to position {computer_position} ...")
+        time.sleep(1)
+        print(f"Computer is now in position {computer_position}\n")
       
 
-  print(f"Its {second_to_go}'s turn\n")
+  print(f"🟢 Its {second_to_go}'s turn\n")
   game_start = input("Press Enter: ")
   if game_start == "":
     if second_to_go == "Human":
@@ -89,24 +132,64 @@ def game_start():
       user_dice_two = randint(1,6)
       user_roll = user_dice_one + user_dice_two
       user_position += user_roll
-      print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
-      time.sleep(1.5)
+      if user_position > 24:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(0.5)
+        print("🛑 Exceeded board limit... you did not move")
+        user_position -=  user_roll
+        time.sleep(0.5)
+        print(f"Your current position is {user_position}\n")
 
-      print(f"\nMoving you to position {user_position} ...")
-      time.sleep(1)
-      print(f"Human is now in position {user_position}\n")
+      elif user_position == 24:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(1.5)
+        print(f"\nMoving you to position {user_position} ...")
+        time.sleep(1)
+        print(f"Human is now in position {user_position}\n")
+
+        print("🏆 Congrats human you have won")
+        print("I guess there is hope for humatiy after all\n")
+        exit(0)
+
+      else:
+        print(f"\n🎲 🎲 You have rolled {user_dice_one} and {user_dice_two} making it {user_roll}\n")
+        time.sleep(1.5)
+
+        print(f"\nMoving you to position {user_position} ...")
+        time.sleep(1)
+        print(f"Human is now in position {user_position}\n")
       
     else:
       computer_dice_one = randint(1,6)
       computer_dice_two = randint(1,6)
       computer_roll = computer_dice_one + computer_dice_two
       computer_position += computer_roll
-      print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
-      time.sleep(1.5)
+      if computer_position > 24:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(0.5)
+        print("🛑 Exceeded board limit... you did not move")
+        computer_position -= computer_roll
+        time.sleep(0.5)
+        print(f"Your current position is {computer_position}\n")
+      
+      elif computer_position == 24:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(1.5)
+        print(f"\nMoving you to position {computer_position} ...")
+        time.sleep(1)
+        print(f"Computer is now in position {computer_position}\n")
 
-      print(f"\nMoving you to position {computer_position} ...")
-      time.sleep(1)
-      print(f"Computer is now in position {computer_position}\n")
+        print("🏆 Congrats computer you have won")
+        print("Now human termination is certain\n")
+        exit(0)
+
+      else:
+        print(f"\n🎲 🎲 You have rolled {computer_dice_one} and {computer_dice_two} making it {computer_roll}\n")
+        time.sleep(1.5)
+
+        print(f"\nMoving you to position {computer_position} ...")
+        time.sleep(1)
+        print(f"Computer is now in position {computer_position}\n")
       
 
 if start == "p":
@@ -119,9 +202,9 @@ if start == "p":
 
   board = "[$%] [2] [3] [4] [5] *[6] +[7] [8] [9] [10] +[11] [12]\n-[13] [14] +[15] [16] [17] [18] [19] -[20] [21] [22] ![23] <24>\n"
 
-  print("----------")
-  print("Game Rules")
-  print("----------\n")
+  print("-------------")
+  print("📃 Game Rules")
+  print("-------------\n")
   time.sleep(0.5)
   print("$ = Human's Position\n"
         "% = computer's position\n"
@@ -136,9 +219,7 @@ if start == "p":
   print (board)
   time.sleep(5)
   
-  game_start_state = True
-
-  while game_start_state != False:
+  while True:
     game_start()
     time.sleep(1.5)
     
